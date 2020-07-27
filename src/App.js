63a4580f0c13ed8745ccf5ee7f3bde6493595aea
Gregory Wilson <gjwilson7390@gmail.com>
2020-07-27 12:42:34 -0500

    added styled-components

--- src/App.js
+++ src/App.js
@@ -1,71 +1,84 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import styled from 'styled-components'
 import PrivateRoute from './components/PrivateRoute'
 import Recipes from './components/Recipes'
-
 import './App.css';
+
+
+
+const ApplicationBox = styled.div`
+
+  margin: 20px;
+  text-align: center;
+  width: 70%;
+  background-color: #EBCD20;
+  border: 5px solid #F26D00;
+  border-radius: 15px;
+  font-family: 'Roboto', sans-serif;
+  margin: 0 auto;
+`
 
 function App() {
   return (
-    <div className="App">
+    
       <Router>
+      <ApplicationBox>
+          <Route exact path='/'>
+          <h1>Register</h1>
+          <div className='form'>
+          <form>
+            <p><input
+                name='name'
+                id='name'
+                placeholder='Full Name Here'
+                type='text'
+            /></p>
 
-      <Route exact path='/'>
-      <h1>Register</h1>
-      <div className='form'>
-      <form>
-        <p><input
-            name='name'
-            id='name'
-            placeholder='Full Name Here'
-            type='text'
-        /></p>
+            <p>
+            <input
+                name='email'
+                id='email'
+                placeholder='Email Address Here'
+                type='email'
+            /></p>
 
-        <p>
-        <input
-            name='email'
-            id='email'
-            placeholder='Email Address Here'
-            type='email'
-        /></p>
+            <p>
+            <input
+                name='passsword'
+                id='password'
+                placeholder='Password Here'
+                type='password'
+             /></p>
+          </form>
+          </div>
+          </Route>
+          
 
-        <p>
-        <input
-            name='passsword'
-            id='password'
-            placeholder='Password Here'
-            type='password'
-        /></p>
-      </form>
-      </div>
-      </Route>
-      
+          <Route path='/login'>
+          <h1>Login</h1>
 
-      <Route path='/login'>
-      <h1>Login</h1>
+            <input
+                name='email'
+                id='email'
+                placeholder='Email Address Here'
+                type='email'
+            />
+            <input
+                name='passsword'
+                id='password'
+                placeholder='Password Here'
+                type='password'
+            />
 
-        <input
-            name='email'
-            id='email'
-            placeholder='Email Address Here'
-            type='email'
-        />
-        <input
-            name='passsword'
-            id='password'
-            placeholder='Password Here'
-            type='password'
-        />
-
-      </Route>
+          </Route>
 
 
-         <PrivateRoute path='/protected' component={Recipes} />
+            <PrivateRoute path='/protected' component={Recipes}></PrivateRoute>
 
+
+      </ApplicationBox>
       </Router>
-
-    </div>
   );
 }
 
