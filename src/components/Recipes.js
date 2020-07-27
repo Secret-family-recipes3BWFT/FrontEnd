df292c812b0f484156e778a31f250cf2c26cca39
Julian Leon <jleon1992@gmail.com>
2020-07-27 15:47:28 -0400

    added form

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {axiosWithAuth} from '../utils/axiosWithAuth'
+import {getRecipes, postRecipe} from '../actions/actions'
+import {connect} from 'react-redux'
+import Recipe from './Recipe'
+import RecipeForm from './RecipeForm'
+import {useHistory} from 'react-router-dom'
 
-export const Recipes = () => {
+
+export const Recipes = props => {
+
+    useEffect(() => {
+        props.getRecipes()
+    },[])
+
+
+ 
     return (
         <div>
-            <h1>Recipes</h1>
+            <h1>Welcome</h1>
+          
+            {/* <RecipeForm /> */}
+            {props.recipes.map(recipe => {                
+                    return(
+                        <Recipe recipe={recipe} />
+                    )                
+            })}
+            <h1>No Recipes?</h1>
+          
+            <RecipeForm   />
         </div>
     )
 }
 
-export default Recipes
+const mapStateToProps = state => ({
+    recipes: state.recipes,
+    error: state.error
+})
+export default connect(mapStateToProps,
+    {getRecipes, postRecipe}
+    )(Recipes)
