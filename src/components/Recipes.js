6bf4a4872259b06be3de5c906df1929f6641a85e
Julian Leon <jleon1992@gmail.com>
2020-07-28 13:11:52 -0400

    searchbar added and form was moved to new route

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,16 +1,28 @@
 import React, {useEffect, useState} from 'react'
 import {axiosWithAuth} from '../utils/axiosWithAuth'
-
+import {useHistory} from "react-router-dom";
 import Recipe from './Recipe'
-import RecipeForm from './RecipeForm'
+import SearchBar from './SearchBar'
 
-import {RecipeContext} from '../contexts/RecipeContext'
+import styled from 'styled-components'
 
+
+const LinkButton = styled.button`
+  border: 0px solid white;
+  margin: 10px;
+  border-radius: 5px;
+  background-color: #F26D00;
+  width: 100px;
+  height: 40px; 
+  color: white;
+  font-family: 'Roboto', sans-serif;
+  font-size: 1.5rem;
+`
 
 export const Recipes = props => {
-
+    const {push} = useHistory()
     const [recipes, setRecipes] = useState([])
-
+    const [search, setSearch] = useState('')
     useEffect(() => {
         axiosWithAuth()
         .get('https://lambdaschool-cookbook2.herokuapp.com/recipes')
@@ -22,22 +34,34 @@ export const Recipes = props => {
             console.log(err)
         })
     },[])
-
     
+    const changeSearch = e => {
+        
+        setSearch(e.target.value)
+        
+    }
+    const searchedRecipes = recipes.filter(recipe => {
+        return recipe.title.indexOf(search) !== -1            
+     }          
+     )
+     console.log(searchedRecipes)
+     console.log(search)
  
     return (
         <div>
             <h1>Welcome</h1>
-          
+
+            <SearchBar changeSearch={changeSearch} />
             {/* <RecipeForm /> */}
-            {recipes.map(recipe => {                
+            {searchedRecipes.map(recipe => {                
                     return(
-                        <Recipe recipe={recipe} />
+                        <Recipe recipe={recipe} searchValue={search} />
                     )                
             })}
             <h1>No Recipes?</h1>
+            <button onClick={()=> {push('/newrecipe')}}>Add New Recipe</button>
           
-            <RecipeForm   />
+            {/* <RecipeForm   /> */}
         </div>
     )
 }
